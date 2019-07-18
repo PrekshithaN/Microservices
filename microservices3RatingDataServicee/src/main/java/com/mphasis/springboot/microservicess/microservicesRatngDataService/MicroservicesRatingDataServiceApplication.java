b95e26f3b29d496f509b0e7b7de3208683bf7340
prekshitha <prekshitha.n@mphasis.com>
2019-07-18 12:37:08 +0530

    commit

---
+++ microservices3RatingDataServicee/src/main/java/com/mphasis/springboot/microservicess/microservicesRatngDataService/MicroservicesRatingDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mphasis.springboot.microservicess.microservicesRatngDataService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesRatingDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesRatingDataServiceApplication.class, args);
+	}
+
+}
